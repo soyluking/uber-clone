fef804e8ce186a39e12d678f2fb9d30204e2f9a8
Luigi <lpizarro@meatcode.cl>
2021-08-01 09:57:30 -0400

    feat: add input destination

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -2,34 +2,33 @@ import React from 'react';
 import { SafeAreaView, StyleSheet, ScrollView, Text, View } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_API_KEY } from '@env';
+import { useNavigation } from '@react-navigation/native';
+import { useDispatch } from 'react-redux';
+import { setDestination } from '../slices/navSlice';
 import tw from 'tailwind-react-native-classnames';
 
 const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
-      <Text style={tw`text-center py-5 text-xl`}>Good Morning, XXX</Text>
+      <Text style={tw`text-center py-5 text-xl`}>Good Morning, Luigi</Text>
 
       <View style={tw`border-t border-gray-200 flex-shrink`}>
         <ScrollView keyboardShouldPersistTaps='always'>
           <GooglePlacesAutocomplete
-            placeholder='Where from?'
-            syles={{
-              container: {
-                flex: 0,
-              },
-              textInput: {
-                fontSize: 18,
-              },
-            }}
+            placeholder='Where to?'
+            styles={toInputBoxStyles}
             onPress={(data, details = null) => {
               dispatch(
-                setOrigin({
+                setDestination({
                   location: details.geometry.location,
                   description: details.description,
                 }),
               );
 
-              dispatch(setDestination(null));
+              navigation.navigate('RideOptionsCard');
             }}
             fetchDetails={true}
             returnKeyType={'search'}
@@ -50,4 +49,19 @@ const NavigateCard = () => {
 
 export default NavigateCard;
 
-const styles = StyleSheet.create({});
+const toInputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    paddingTop: 20,
+    flex: 0,
+  },
+  textInput: {
+    backgroundColor: '#dddddf',
+    borderRadius: 0,
+    fontSize: 18,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  },
+});
